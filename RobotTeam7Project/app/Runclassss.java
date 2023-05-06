ba2869393968877ea5167a91ce1fb16584778b90
Fatemeh001 <fatemeh22001@student.hamk.fi>
2023-05-06 16:51:35 +0300

    runclasss

---
+++ RobotTeam7Project/app/Runclassss.java
@@ -0,0 +1,15 @@
+package app;
+
+public class Runclassss extends Thread {
+
+  public static void main(String[] args) {
+    Motors motorsObj = new Motors();
+    LineFollow LFObj = new LineFollow();
+
+    Thread thread1 = new Thread(LFObj);
+    Thread thread3 = new Thread(motorsObj);
+
+    thread1.start();
+    thread3.start();
+  }
+}
